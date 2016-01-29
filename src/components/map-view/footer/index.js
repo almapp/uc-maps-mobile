809bfeb359b7c60c03723bdd72563d5556a34079
Patricio López <lopezjuripatricio@gmail.com>
2016-01-29 20:44:23 -0300

    replace swiper with view pager and remove unused imports

--- src/components/map-view/footer/index.js
+++ src/components/map-view/footer/index.js
@@ -1,16 +1,17 @@
 import React, { StyleSheet, Text, View, Component } from 'react-native'
-import Swiper from 'react-native-swiper'
+import ViewPager from 'react-native-viewpager'
 import Button from 'react-native-button'
 import Icon from 'react-native-vector-icons/Ionicons'
 
 import Colors from '../../../global/colors'
 
-const HEIGHT = 85;
-const TAB_HEIGHT = 39
 
 export default class Footer extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      selected: 0,
+    }
   }
 
   static get defaultProps() {
@@ -19,57 +20,74 @@ export default class Footer extends Component {
     }
   }
 
+  get datasource() {
+    return new ViewPager.DataSource({ pageHasChanged: (p1, p2) => p1.identifier !== p2.identifier })
+  }
+
   render() {
+    const datasource = this.datasource.cloneWithPages(this.props.areas)
+
     return (
       <View style={styles.view.container}>
 
-        <Swiper ref="swiper" style={styles.swiper.container} height={HEIGHT} showsButtons={true} showsPagination={false} onMomentumScrollEnd={this.selectArea.bind(this)}>
-          {this.props.areas.map((area, i) => (
-            <View key={i} style={styles.swiper.slide}>
-              <Text style={styles.swiper.title} numberOfLines={2}>{area.shortName}</Text>
-              <Text style={styles.swiper.detail}>a 10 minutos</Text>
-            </View>
-          ))}
-        </Swiper>
-
-        <View style={styles.selector.container}>
-          <Button style={styles.selector.details} onPress={this.showDetails.bind(this)}>Detalles</Button>
-          <Button style={styles.selector.classrooms} onPress={this.showClassrooms.bind(this)}>Salas</Button>
-          <Button style={styles.selector.classrooms} onPress={this.showServices.bind(this)}>Servicios</Button>
-        </View>
+        <ViewPager
+          style={styles.swiper.container}
+          dataSource={datasource}
+          renderPage={this.renderPage.bind(this)}
+          isLoop={true}
+          onChangePage={this.selectArea.bind(this)}
+          autoPlay={false}/>
 
     </View>
     )
   }
 
+  renderPage(area, page) {
+    return (
+      <View style={styles.swiper.slide}>
+        <View style={styles.swiper.texts}>
+          <Text style={styles.swiper.title} numberOfLines={2}>{area.shortName}</Text>
+          <Text style={styles.swiper.detail}>a 10 minutos</Text>
+        </View>
+
+        <View style={styles.swiper.selector}>
+          <Button style={styles.swiper.button} onPress={this.showDetails.bind(this)}>Detalles</Button>
+          <Button style={styles.swiper.button} onPress={this.showClassrooms.bind(this)}>Salas</Button>
+          <Button style={styles.swiper.button} onPress={this.showServices.bind(this)}>Servicios</Button>
+        </View>
+      </View>
+    )
+  }
+
   showDetails() {
-    // TODO: get index
-    const index = 0
+    const index = this.state.selected
     if (this.props.onShowDetails) this.props.onShowDetails(this.props.areas[index])
   }
 
   showClassrooms() {
-    // TODO: get index
-    const index = 0
+    const index = this.state.selected
     if (this.props.onShowClassrooms) this.props.onShowClassrooms(this.props.areas[index])
   }
 
   showServices() {
-    // TODO: get index
-    const index = 0
+    const index = this.state.selected
     if (this.props.onShowServices) this.props.onShowServices(this.props.areas[index])
   }
 
-  selectArea(e, state, context) {
-    const index = state.index
+  selectArea(index) {
+    this.setState({
+      selected: index,
+    })
     if (this.props.onAreaSelection) this.props.onAreaSelection(this.props.areas[index])
   }
 }
 
+const SIDE_MARGIN = 40
+
 const styles = {
   view: StyleSheet.create({
     container: {
-      flexDirection: 'column',
+      paddingBottom: 4,
       backgroundColor: 'white',
       shadowOffset: {
         width: 0,
@@ -81,67 +99,42 @@ const styles = {
     },
   }),
 
-  tabView: StyleSheet.create({
-    container: {
-      backgroundColor: Colors.MAIN,
-      height: TAB_HEIGHT,
-
-    },
-    tabs: {
-      backgroundColor: Colors.MAIN,
-      height: TAB_HEIGHT,
-      paddingBottom: 1,
-    },
-    tab: {
-      // backgroundColor: Colors.MAIN, //'rgba(0,0,0,0)',
-    },
-  }),
-
   swiper: StyleSheet.create({
     container: {
-      shadowOffset: {
-        width: 0,
-        height: -2,
-      },
-      shadowRadius: 1,
-      shadowColor: 'black',
-      shadowOpacity: 0.9,
+      height: 420,
     },
     slide: {
       flex: 1,
+      flexDirection: 'column',
       backgroundColor: 'white',
+      justifyContent: 'space-between',
+    },
+    texts: {
+      height: 75,
     },
     title: {
       fontSize: 19,
       fontWeight: '500',
-      marginLeft: 40,
-      marginTop: 10,
-      marginRight: 40,
+      marginLeft: SIDE_MARGIN,
+      marginTop: 12,
+      marginRight: SIDE_MARGIN,
     },
     detail: {
-      marginLeft: 40,
+      marginLeft: SIDE_MARGIN,
       marginTop: 3,
       fontWeight: '200',
     },
-  }),
-
-  selector: StyleSheet.create({
-    container: {
-      height: 30,
-      marginBottom: 12,
+    selector: {
       flexDirection: 'row',
       alignItems: 'center',
-      marginLeft: 10,
-      marginRight: 10,
-      justifyContent: 'space-around',
+      marginBottom: 30,
+      marginTop: 13,
+      marginLeft: SIDE_MARGIN,
+      marginRight: SIDE_MARGIN,
+      justifyContent: 'space-between',
     },
-    details: {
+    button: {
       color: Colors.COMPLEMENT,
-      // paddingLeft: 25,
-    },
-    classrooms: {
-      color: Colors.COMPLEMENT,
-      // paddingLeft: 25,
     },
   }),
 }
