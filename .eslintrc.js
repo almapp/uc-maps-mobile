cd57627bbef9e2de87f47ae8c3e32d7503552a8e
Patricio López <lopezjuripatricio@gmail.com>
2016-03-03 21:48:08 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+    },
+    //"extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true,
+        },
+        "sourceType": "module",
+    },
+    "plugins": [
+        "react",
+    ],
+    "extends": "plugin:react/recommended",
+    "globals": {
+      "fetch": true,
+    },
+    "rules": {
+        "indent": [
+            2,
+            2,
+        ],
+        "linebreak-style": [
+            2,
+            "unix",
+        ],
+        "quotes": [
+            2,
+            "single",
+            "avoid-escape",
+        ],
+        "semi": [
+            2,
+            "never",
+        ],
+        "comma-dangle": [
+          2,
+          "always-multiline",
+        ],
+        "react/prop-types": [
+          0, // hide for now
+        ]
+    },
+};
